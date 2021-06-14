899a56faeb055b8e4bfae5f66520192ab5d06f0f
prasadpalase <prasadp.palase@gmail.com>
2021-06-14 16:24:40 +0530

    array_diff commit  on files

--- Array in php/array_elements.php
+++ Array in php/array_elements.php
@@ -6,4 +6,8 @@ $array = [0 => "a", 1 => "ccc", 2 => "c"];
 
 array_splice($array, 1, 1);
 
+
+
+$array = [0 => "a", 1 => "b", 2 => "c", 3 => "c"];
+$array = array_diff($array, ["a", "c"]);
 // ↑ Offset which you want to delete
