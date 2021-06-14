343060004f60028f1ab983641855fa2ea660e892
prasadpalase <prasadp.palase@gmail.com>
2021-06-14 15:56:58 +0530

    array function add

--- Array in php/array_elements.php
+++ Array in php/array_elements.php
@@ -4,3 +4,8 @@ unset($array[1]);
 
 print_r($array);
 // ↑ Key which you want to delete
+$array = [0 => "a", 1 => "b", 2 => "c"];
+
+array_splice($array, 1, 1);
+
+// ↑ Offset which you want to delete
